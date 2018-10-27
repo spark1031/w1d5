e9f4d8a78ed3c012b282cd5689014b307fda68a4
Sujin Park <sujinpark1031@gmail.com>
2018-10-27 09:53:41 -0700

    Reorganize KnightPathFinder class

--- skeleton/lib/01_knight_path_finder.rb
+++ skeleton/lib/01_knight_path_finder.rb
@@ -2,21 +2,32 @@ require_relative '00_tree_node'
 
 class KnightPathFinder
     attr_reader :root_node
-  
+
   def initialize(pos)
     @root_node = PolyTreeNode.new(pos)
     @visited_positions = [pos]
-  
-    
-    self.build_move_tree #do we need to store this as an instance variable??
+    build_move_tree #do we need to store this as an instance variable??
   end
-  
+
+  def find_path(end_pos)
+    target_node = @root_node.bfs(end_pos)
+    parent_node = target_node.parent
+    results = [target_node.value]
+
+    until parent_node.nil?
+      results.unshift(parent_node.value)
+      parent_node = parent_node.parent
+    end
+    results
+  end
+
+  private
   # returns an array of positions: new_move_positions(current.value)
-  # iterate over my positions and make them into node objects (PolyTreeNode.new(pos)) - 
+  # iterate over my positions and make them into node objects (PolyTreeNode.new(pos)) -
     # current_node.add_child(child_node)
     # shovel these node objects into queue array
     # set parent to current_node for each child_node
-  def build_move_tree #take in @root_node as an argument - we have some logic that builds out all the possible positions
+  def build_move_tree
     queue = [@root_node]
     until queue.empty?
       current_node = queue.shift
@@ -29,9 +40,8 @@ class KnightPathFinder
       end
     end
   end
-  
 
-  def self.valid_moves(pos) 
+  def self.valid_moves(pos)
     pre_result = []
     (-2..2).each do |adder1|
       (-2..2).each do |adder2|
@@ -42,32 +52,27 @@ class KnightPathFinder
     end
     pre_result.select { |potential_pos| potential_pos.first.between?(0, 7) && potential_pos.last.between?(0, 7) }
   end
-  
-  #this calls ::valid_moves but filters out positions inside @visited_positions (it's an array)
+
   def new_move_positions(pos)
     all_moves = KnightPathFinder.valid_moves(pos)
-    
-    filtered_moves = all_moves - @visited_positions 
-    @visited_positions += filtered_moves 
+
+    filtered_moves = all_moves - @visited_positions
+    @visited_positions += filtered_moves
     return filtered_moves
-  end 
-  
-  def find_path(end_pos)
-    target_node = @root_node.bfs(end_pos)
-    parent_node = target_node.parent 
-    results = [target_node.value]
-    
-    until parent_node.nil? 
-      results.unshift(parent_node.value)
-      parent_node = parent_node.parent
-    end 
-    
-    results 
-  end 
-  
+  end
 
 end
 
 
+if __FILE__ = $0
+  new_kpf = KnightPathFinder.new([0,0])
+  new_kpf.find_path([4,2])
+  KnightPathFinder.valid_moves([4,3]) #this method has not relation to new_kpf
 
+end
 
+#a class method does not rely on us making a instance object
+#of our class prior to using it since our class method's
+#functionality does not depend on a specific class object.
+#e.g. if we look above, finding the valid moves for [4,3] does
+#not depend on the relationships built out in new_kpf
